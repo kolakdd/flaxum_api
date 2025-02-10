1821d93ce6750d4ffa0af2ebab939a64364dbe3b
kolakdd <kolakdd18@gmail.com>
2025-02-11 02:44:31 +0700

    refactor: 2
    mend

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,11 +1,10 @@
 pub mod config;
 pub mod domain;
-pub use error::Error;
+pub mod error;
 pub mod common;
 pub mod db;
 pub mod logger;
 pub mod scalar;
 pub mod state;
 pub mod utils;
-
-mod error;
+pub mod response;
